3ba2d19e87ffbcfb7874bf8f07ff0e306beb627f
Theodore Li <theodoreqili@gmail.com>
2025-03-26 22:07:49 -0700

    Throw 400 on merchant display name not being set during create transfer request (#5)
    * Throw 400 on merchant display name not being set during create transfer request
    
    * Remove cognito util from create transfer request
    
    ---------
    
    Co-authored-by: Theodore Li <teddy@zenobiapay.com>

--- kotlin/lambda/transfer-handler/src/main/kotlin/com/zenobiapay/transfer/operations/CreateTransferRequestOperation.kt
+++ kotlin/lambda/transfer-handler/src/main/kotlin/com/zenobiapay/transfer/operations/CreateTransferRequestOperation.kt
@@ -19,13 +19,13 @@ class CreateTransferRequestOperation @Inject constructor(
     private val transferDao: TransferDao,
     private val userDao: UserDao,
     private val objectMapper: ObjectMapper,
-    private val cognitoUtil: CognitoUtil
 ): Operation() {
     override fun run(input: APIGatewayProxyRequestEvent, context: Context, userId: String?): Any {
         val request = objectMapper.readValue<CreateTransferRequestRequest>(input.body)
 
         val requestId = input.requestContext.requestId
-        val merchantName = userDao.getMerchant(userId!!)!!.data.displayName ?: cognitoUtil.getUserFullName(userId)
+        val merchantName = userDao.getMerchant(userId!!)?.data?.displayName
+            ?: throw InvalidRequestException("Merchant display name not configured.")
         transferDao.putTransferRequest(
             userId,
             requestId,
