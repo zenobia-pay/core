635d38284c4fa9befb84803156cd1943352c1847
Theodore Li <teddy@zenobiapay.com>
2025-04-02 21:19:05 -0700

    List transfers are all posts*

--- kotlin/lambda/user-handler/src/main/kotlin/com/zenobiapay/user/operations/CreateM2MCredentialsOperation.kt
+++ kotlin/lambda/user-handler/src/main/kotlin/com/zenobiapay/user/operations/CreateM2MCredentialsOperation.kt
@@ -5,23 +5,31 @@ import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent
 import com.zenobiapay.api.generated.models.CreateM2mCredentials200Response
 import com.zenobiapay.api.model.Operation
 import com.zenobiapay.api.model.cognito.UserPoolGroup
+import com.zenobiapay.table.user.dao.UserDao
 import com.zenobiapay.user.util.Auth0Wrapper
 import com.zenobiapay.user.util.Auth0Wrapper.Companion.ZENOBIA_AUDIENCE
+import io.github.oshai.kotlinlogging.KotlinLogging
 import javax.inject.Inject
 
-class CreateM2MCredentialsOperation @Inject constructor(private val auth0Wrapper: Auth0Wrapper): Operation() {
+private val logger = KotlinLogging.logger {}
+
+class CreateM2MCredentialsOperation @Inject constructor(private val auth0Wrapper: Auth0Wrapper, private val userDao: UserDao): Operation() {
     override fun run(
         input: APIGatewayProxyRequestEvent,
         context: Context,
         userId: String?
     ): Any {
         userId!!
-        val createdClient = auth0Wrapper.createClientCredentials(userId)
-        auth0Wrapper.createClientGrant(createdClient, ZENOBIA_AUDIENCE)
+        val client = auth0Wrapper.createClientCredentials(userId)
+        logger.info { "Created client credentials for user id $userId" }
+        auth0Wrapper.createClientGrant(client, ZENOBIA_AUDIENCE)
+
+        userDao.putM2MCredentials(userId, client.clientId, client.name)
+        logger.info { "Recorded m2m credentials in ddb table"}
 
         return CreateM2mCredentials200Response(
-            clientId = createdClient.clientId,
-            clientSecret = createdClient.clientSecret
+            clientId = client.clientId,
+            clientSecret = client.clientSecret
         )
     }
 
