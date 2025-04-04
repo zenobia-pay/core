e8abae2af9a36fc431a218ee4cb1685ee710c771
Theodore Li <teddy@zenobiapay.com>
2025-04-04 16:57:05 -0700

    Remove action app

--- terraform/main.tf
+++ terraform/main.tf
@@ -35,14 +35,6 @@ resource "auth0_client_credentials" "zenobia_app_credentials" {
   authentication_method = "none"
 }
 
-resource "auth0_client" "auth0_action_app" {
-  name            = "auth0-action-app-tf"
-  description     = "Used by Auth0 Actions to call API Gateway"
-  app_type        = "non_interactive"
-  grant_types     = ["client_credentials"]
-  is_first_party  = true
-}
-
 resource "auth0_client_grant" "auth0_action_client_grant" {
   client_id = auth0_client.auth0_action_app.client_id
   audience  = auth0_resource_server.zenobia_api.identifier
