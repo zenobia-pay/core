51779101a49eb1c97675c5c25a30e1db7a9aeccb
Theodore Li <teddy@zenobiapay.com>
2025-04-22 15:55:04 -0700

    Add instant payouts on fulfill

--- kotlin/lambda/payout-handler/src/main/kotlin/com/zenobiapay/payout/handlers/PayoutProcessor.kt
+++ kotlin/lambda/payout-handler/src/main/kotlin/com/zenobiapay/payout/handlers/PayoutProcessor.kt
@@ -2,6 +2,7 @@ package com.zenobiapay.payout.handlers
 
 import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.RequestHandler
+import com.amazonaws.services.lambda.runtime.events.DynamodbEvent
 import com.amazonaws.services.lambda.runtime.events.SQSEvent
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.zenobiapay.orum.OrumWrapper
@@ -10,8 +11,13 @@ import com.zenobiapay.orum.model.OrumCreateTransferRequest
 import com.zenobiapay.orum.model.TransferParticipant
 import com.zenobiapay.orum.util.generateMerchantOrumId
 import com.zenobiapay.payout.di.DaggerAppComponent
+import com.zenobiapay.payout.model.EventBridgeEvent
 import com.zenobiapay.payout.model.PayoutMessage
+import com.zenobiapay.payout.model.ScheduledEvent
+import com.zenobiapay.payout.util.EventBridgeEventSerializer
 import com.zenobiapay.table.transfer.dao.TransferDao
+import com.zenobiapay.table.transfer.model.OutboundTransferStatus
+import com.zenobiapay.table.transfer.model.TransferItem
 import com.zenobiapay.table.transfer.util.getFee
 import com.zenobiapay.table.user.dao.UserDao
 import com.zenobiapay.table.user.model.UserType
@@ -22,7 +28,7 @@ private val logger = KotlinLogging.logger {}
 
 class MerchantNotConfiguredException: Exception("Merchant configuration not set!")
 
-class PayoutProcessor : RequestHandler<SQSEvent, Unit> {
+class PayoutProcessor : RequestHandler<Map<String, Any>, Unit> {
 
     @Inject
     lateinit var transferDao: TransferDao
@@ -36,40 +42,55 @@ class PayoutProcessor : RequestHandler<SQSEvent, Unit> {
     @Inject
     lateinit var objectMapper: ObjectMapper
 
+    @Inject
+    lateinit var eventBridgeEventSerializer: EventBridgeEventSerializer
+
     init {
         DaggerAppComponent.create().inject(this)
     }
 
-    override fun handleRequest(event: SQSEvent, context: Context?) {
-        logger.info { "Got event $event" }
-        logger.info { "Got context $context" }
-
-        event.records.map {
-            objectMapper.readValue(it.body, PayoutMessage::class.java).also {
-                logger.info { "Got record $it" }
-            }
-        }.forEach {
-            fulfillPayout(it)
-        }
-    }
-
-    private fun fulfillPayout(message: PayoutMessage) {
-        val payoutItem = transferDao.getPayoutItem(message.merchantId, message.date)
-        if (payoutItem == null) {
-            logger.info { "Did not find payout item for merchant ${message.merchantId}. Skipping paying out" }
+    override fun handleRequest(event: Map<String, Any>, context: Context?) {
+//        logger.info { "Got event $event" }
+        val request = objectMapper.convertValue(event, EventBridgeEvent::class.java)
+        if (request.detail?.eventName != "MODIFY") {
+            logger.info { "Request is not a modification, ignoring." }
             return
         }
 
-        assert(payoutItem.data?.merchantPaid != true && payoutItem.data?.feePaid != true) {
-            "Payout item pk=${payoutItem.pk}, sk=${payoutItem.sk} should not have merchant paid or fee paid!"
+        val oldImage = request.detail?.dynamodb?.oldImage?.let {
+            TransferItem.fromAttributeValueMap(
+                eventBridgeEventSerializer.parseAttributeValueMap(it)
+            )
+        }
+        val newImage = request.detail?.dynamodb?.newImage?.let {
+            TransferItem.fromAttributeValueMap(
+                eventBridgeEventSerializer.parseAttributeValueMap(it)
+            )
         }
 
-        val fee = getFee(payoutItem.amount)
-        val merchantPayout = payoutItem.amount - fee
+        logger.info { "Got transfer request id ${newImage?.requestId}"}
 
-        val merchantData = userDao.getUserItem(message.merchantId)
+        if (newImage?.outboundStatus != OutboundTransferStatus.IN_FLIGHT) {
+            logger.info { "Request is not in flight. Skipping paying out"}
+        }
+        val transferItem = transferDao.getTransfer(newImage!!.requestId) ?: throw Error("Could not find transfer item ${newImage.requestId}")
+        fulfillPayout(transferItem)
+    }
+
+    private fun fulfillPayout(transferItem: TransferItem) {
+        if (transferItem.outboundStatus != OutboundTransferStatus.IN_FLIGHT) {
+            logger.info { "Outbound transfer status is not in flight. Skipping paying out." }
+        }
+
+        val amount = transferItem.amount!!
+        val fee = getFee(amount)
+        val merchantPayout = amount - fee
+        val merchantId = transferItem.data?.merchant?.id!!
+
+        assert(merchantPayout > 0)
+        val merchantData = userDao.getUserItem(merchantId)
         assert(merchantData?.userType == UserType.MERCHANT) {
-            "Merchant data for merchant ${message.merchantId} does not exist or is not a merchant"
+            "Merchant data for merchant ${merchantData?.pk} does not exist or is not a merchant"
         }
         val merchantBankAccountId = merchantData?.data?.merchantData?.bankAccountId
         assert(merchantBankAccountId != null) { "Merchant bank account id is not specified" }
@@ -77,25 +98,24 @@ class PayoutProcessor : RequestHandler<SQSEvent, Unit> {
         // TODO: check the bank account is of correct type
         val bankAccountId = merchantData?.data?.merchantData?.bankAccountId ?: throw MerchantNotConfiguredException()
 
-        // TODO: handle payouts less than 10 cents
+        logger.info { "Locking transfer payout" }
+        transferDao.updateTransferPayoutLocked(transferItem)
+
+        logger.info { "Sending orum payout response" }
         val transferResponse = orumWrapper.createTransfer(
             OrumCreateTransferRequest(
-                transferReferenceId = "${payoutItem.pk}#${payoutItem.sk}",
+                transferReferenceId = "PAYOUT#${transferItem.requestId}",
                 amount = merchantPayout,
                 destination = TransferParticipant(
-                    customerReferenceId = generateMerchantOrumId(message.merchantId),
+                    customerReferenceId = generateMerchantOrumId(merchantId),
                     accountReferenceId = bankAccountId,
-                    statementDisplayName = "ZP_${message.date}"
+                    statementDisplayName = merchantData.data.merchantData?.displayName?.filter { it.isLetterOrDigit() }?.take(10) ?: "ZenobiaPay"
                 )
-            )
+            ),
+            waitForCompletedState = true
         )
-        transferDao.updatePayoutMetadata(
-            payoutItem,
-            merchantPaid = true,
-            merchantAmount = merchantPayout,
-            merchantPayoutId = PayoutId(id = transferResponse.transfer.id),
-            feePaid = false,
-            feeAmount = fee
-        )
+
+        logger.info { "Payout complete. Marking transfer as paid out." }
+        transferDao.updateTransferPaidOut(transferItem, fee, transferResponse.transfer.id, version = transferItem.version!! + 1)
     }
 }
