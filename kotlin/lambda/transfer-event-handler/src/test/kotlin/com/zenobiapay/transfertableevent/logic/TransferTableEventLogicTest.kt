2b65b7c893e96c1adf5ddaee02093447aa30fa14
Theodore Li <teddy@zenobiapay.com>
2025-04-29 14:37:44 -0700

    Send updates for failed status

---
+++ kotlin/lambda/transfer-event-handler/src/test/kotlin/com/zenobiapay/transfertableevent/logic/TransferTableEventLogicTest.kt
@@ -0,0 +1,106 @@
+package com.zenobiapay.transfertableevent.logic
+
+import com.zenobiapay.api.generated.model.TransferStatus
+import com.zenobiapay.table.transfer.model.InboundTransferStatus
+import com.zenobiapay.table.transfer.model.OutboundTransferStatus
+import com.zenobiapay.table.transfer.model.TransferItem
+import com.zenobiapay.transfertableevent.util.WebhookUtil
+import com.zenobiapay.transfertableevent.util.WebsocketUtil
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.jupiter.api.Test
+
+class TransferTableEventLogicTest {
+    val webhookUtil = mockk<WebhookUtil>(relaxed = true)
+    val websocketUtil = mockk<WebsocketUtil>(relaxed = true)
+
+    @Test
+    fun `test send event when payment completes`() {
+        val logic = TransferTableEventLogic(webhookUtil, websocketUtil)
+        val oldImage = createTransferItem(InboundTransferStatus.IN_FLIGHT, OutboundTransferStatus.IN_FLIGHT_APPROVED)
+        val newImage = createTransferItem(InboundTransferStatus.IN_FLIGHT, OutboundTransferStatus.COMPLETED)
+        logic.handleRecord(
+            oldImage,
+            newImage
+        )
+
+        verify {
+            websocketUtil.sendWebsocketUpdate(any(), any(), TransferStatus.COMPLETED)
+        }
+    }
+
+    @Test
+    fun `test send event when payment is in flight`() {
+        val logic = TransferTableEventLogic(webhookUtil, websocketUtil)
+        val oldImage = createTransferItem(InboundTransferStatus.IN_FLIGHT, OutboundTransferStatus.FULFILL_LOCKED)
+        val newImage = createTransferItem(InboundTransferStatus.IN_FLIGHT, OutboundTransferStatus.IN_FLIGHT_WAITING)
+        logic.handleRecord(
+            oldImage,
+            newImage
+        )
+
+        verify {
+            websocketUtil.sendWebsocketUpdate(any(), any(), TransferStatus.IN_FLIGHT)
+        }
+    }
+
+    @Test
+    fun `test send event when payment fails and was waiting`() {
+        val logic = TransferTableEventLogic(webhookUtil, websocketUtil)
+        val oldImage = createTransferItem(InboundTransferStatus.IN_FLIGHT, OutboundTransferStatus.IN_FLIGHT_WAITING)
+        val newImage = createTransferItem(InboundTransferStatus.FAILED, OutboundTransferStatus.IN_FLIGHT_WAITING)
+        logic.handleRecord(
+            oldImage,
+            newImage
+        )
+
+        verify {
+            websocketUtil.sendWebsocketUpdate(any(), any(), any())
+        }
+    }
+
+    @Test
+    fun `test doesnt send event when payment already complete but inbound failed`() {
+        val logic = TransferTableEventLogic(webhookUtil, websocketUtil)
+        val oldImage = createTransferItem(InboundTransferStatus.IN_FLIGHT, OutboundTransferStatus.COMPLETED)
+        val newImage = createTransferItem(InboundTransferStatus.FAILED, OutboundTransferStatus.COMPLETED)
+        logic.handleRecord(
+            oldImage,
+            newImage
+        )
+
+        verify(exactly = 0) {
+            websocketUtil.sendWebsocketUpdate(any(), any(), any())
+        }
+    }
+
+    @Test
+    fun `test doesnt send event when no status changed`() {
+        val logic = TransferTableEventLogic(webhookUtil, websocketUtil)
+        val oldImage = createTransferItem(InboundTransferStatus.FAILED, OutboundTransferStatus.IN_FLIGHT_WAITING)
+        val newImage = createTransferItem(InboundTransferStatus.FAILED, OutboundTransferStatus.IN_FLIGHT_WAITING)
+        logic.handleRecord(
+            oldImage,
+            newImage
+        )
+
+        verify(exactly = 0) {
+            websocketUtil.sendWebsocketUpdate(any(), any(), TransferStatus.FAILED)
+        }
+    }
+
+    private fun createTransferItem(
+        inboundStatus: InboundTransferStatus,
+        outboundStatus: OutboundTransferStatus,
+    ): TransferItem {
+        val mockTransferItem = mockk<TransferItem>(relaxed = true)
+        every {
+            mockTransferItem.inboundStatus
+        } returns inboundStatus
+        every {
+            mockTransferItem.outboundStatus
+        } returns outboundStatus
+        return mockTransferItem
+    }
+}
