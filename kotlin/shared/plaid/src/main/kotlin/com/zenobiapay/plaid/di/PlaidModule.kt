11ac3ef3cb1bcc8fb9f8573416bf18343c9278c7
Theodore Li <teddy@zenobiapay.com>
2025-04-28 22:50:08 -0700

    Use production if plaid endpoint is /production

--- kotlin/shared/plaid/src/main/kotlin/com/zenobiapay/plaid/di/PlaidModule.kt
+++ kotlin/shared/plaid/src/main/kotlin/com/zenobiapay/plaid/di/PlaidModule.kt
@@ -6,14 +6,23 @@ import com.plaid.client.request.PlaidApi
 import com.zenobiapay.plaid.model.PlaidCredentials
 import dagger.Module
 import dagger.Provides
+import io.github.oshai.kotlinlogging.KotlinLogging
 import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient
+
+private val logger = KotlinLogging.logger {}
 
 @Module
 class PlaidModule {
     @Provides
     fun providePlaidApi(plaidCredentials: PlaidCredentials): PlaidApi {
         val apiClient = ApiClient(plaidCredentials.toMap())
-        apiClient.setPlaidAdapter(ApiClient.Sandbox) // TODO: make this interchangeable
+        if (plaidCredentials.endpoint == "/production") {
+            logger.info { "Using production endpoint" }
+            apiClient.setPlaidAdapter(ApiClient.Production)
+        } else {
+            logger.info { "Using sandbox endpoint" }
+            apiClient.setPlaidAdapter(ApiClient.Sandbox)
+        }
 
         return apiClient.createService(PlaidApi::class.java)
     }
