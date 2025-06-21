2049df18843303b7712214371b9be0bc6345a9e8
Theodore Li <teddy@zenobiapay.com>
2025-06-20 22:04:07 -0700

    Fix reseller endpoint

--- kotlin/lambda/item-handler/src/main/kotlin/com/zenobiapay/item/util/ResaleUtil.kt
+++ kotlin/lambda/item-handler/src/main/kotlin/com/zenobiapay/item/util/ResaleUtil.kt
@@ -52,7 +52,7 @@ class ResaleUtil @Inject constructor(
             
             // Create HTTP request
             val request = HttpRequest.newBuilder()
-                .uri(URI.create("http://localhost:8787/api/depop/listings"))
+                .uri(URI.create("http://reseller.zenobiapay.com/api/depop/listings"))
                 .header("Content-Type", "application/json")
                 .POST(HttpRequest.BodyPublishers.ofString(jsonPayload))
                 .build()
