9ecef933e04c4e81bb371e258e827c8bd8cff016
Theodore Li <teddy@zenobiapay.com>
2025-07-08 16:13:07 -0700

    Fix log4j (for real)

--- kotlin/lambda/bank-handler/build.gradle.kts
+++ kotlin/lambda/bank-handler/build.gradle.kts
@@ -104,6 +104,10 @@ tasks {
             exclude(dependency("com.fasterxml.jackson.module:jackson-module-kotlin:.*"))
             // Exclude DynamoDB enhanced client classes from minimization
             exclude(dependency("software.amazon.awssdk:dynamodb-enhanced:.*"))
+            // Exclude Log4j and SLF4J classes from minimization
+            exclude(dependency("org.apache.logging.log4j:log4j-core:.*"))
+            exclude(dependency("org.apache.logging.log4j:log4j-slf4j2-impl:.*"))
+            exclude(dependency("org.slf4j:slf4j-api:.*"))
         }
         
         // Merge service files to avoid duplication
@@ -124,8 +128,11 @@ tasks {
         exclude("org/openjdk/tools/**")
         exclude("com/google/googlejavaformat/**")
         
+        // Make sure META-INF/services are properly included
+        relocate("META-INF/services", "META-INF/services")
+        
         manifest {
-            attributes(mapOf("Main-Class" to "com.zenobiapay.user.handlers.UserHandler"))
+            attributes(mapOf("Main-Class" to "com.zenobiapay.bank.handlers.BankHandler"))
         }
     }
     
